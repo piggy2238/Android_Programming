d48d25f5d47cf20eca10923dfe727fe5c8cdd411
Ooo工友ooO <r00621109@ntu.edu.tw>
2016-07-19 16:08:51 +0800

    Fix Bug It Can Work Now. Two setContentview may crash the App.

--- app/src/main/java/com/example/user/cyanapplication/MainActivity.java
+++ app/src/main/java/com/example/user/cyanapplication/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.user.cyanapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//import android.util.Log;
-//import android.widget.Toast;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     final static String DebugTag="MainActivity";
@@ -11,10 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) { //Constructor
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.new_layout);
-//        Log.d(DebugTag,"HAHAHA");
+//        setContentView(R.layout.new_layout);
+        Log.d(DebugTag,"HAHAHA");
 //        //Log.i(DebugTag,"hehehe");
-//        Log.d(DebugTag,"WAWAWA");
-//        Toast.makeText(this, "This is a test", Toast.LENGTH_LONG).show();
+        Log.d(DebugTag,"WAWAWA");
+        Toast.makeText(this, "This is a test", Toast.LENGTH_LONG).show();
     }
 }
